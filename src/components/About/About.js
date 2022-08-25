6ef8332ab594f104e6d56770859a2e833ef73c1d
otarampinelli <otaviorampinellielias@gmail.com>
2022-08-25 10:54:08 -0300

    feat: added new infos in about session

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -10,9 +10,14 @@ const About = () => {
             </div>
             <div className='about-post'>
                 <h2>Prazer! &#128579;</h2>
-                <p>Me chamo Otavio, sou desenvolvedor de software - e atualmente trabalho como fullstack na <a href="https://taller.net.br">Taller</a>, uma empresa focada em negócios digitais.
+                <p>Me chamo Otavio, sou desenvolvedor de software - e atualmente trabalho como fullstack na <a href="https://taller.net.br">Taller</a>.
                 </p>
+                <p>Sou ciclista e baterista nas horas vagas, apaixonado por cozinhar, por música e principalmente por não fazer nada quando tenho oportunidade.</p>
+                <iframe title="spotify-frame" style={{borderRadius: "12px"}} src="https://open.spotify.com/embed/album/2cUpAOlQjV5uSjkWj5bEQY?utm_source=generator" width="100%" height="80" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
             </div>
+
+
+            
         </section>
     )
 }
